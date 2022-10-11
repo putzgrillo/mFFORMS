670ecd60f595705582e507eb1f5e85978211a42b
Bruno Grillo <bruno.grillo@ufrgs.br>
2022-10-10 21:09:30 -0300

    trying offline for gcp

--- R/aCentral_Script.R
+++ R/aCentral_Script.R
@@ -31,26 +31,31 @@ m4_data <- m4_data %>%
   mutate(min_obs = ifelse(frequency == 1, h, 2 * frequency)) %>%
   filter(n > ( min_obs + 2*h)) %>%
   # filter(frequency == 24) %>%
-  sample_n(3500)
+  sample_n(500)
 
 # offline data phase ----
 t0 <- Sys.time()
-m4_data <- offline_data_phase(.data = m4_data, .n_cores = 3)
+m4_data <- offline_data_phase(.data = m4_data, .n_cores = 3, .series_per_chunk = 5, .temp_file = 'teste_save.rds', .save_each = 3)
 Sys.time() - t0
 
-# saveRDS(m4_data, file = 'db/m4_post_offline.rds', compress = FALSE)
-saveRDS(m4_data, file = 'db/m4_post_offline_2.rds', compress = FALSE)
+saveRDS(m4_data, file = 'db/m4_post_offline.rds', compress = FALSE)
 
 # # validação erros
 # data_testar <- data %>% filter(!(id %in% data$id)) # m4_data depois de validado
+# data_testar <- m4_data %>% filter(!(id %in% m4_offline$id))
 # teste <- vector('list', nrow(data_testar))
 # Sys.time() # começo
-# for (w in seq_along(teste)) {
+# for (w in seq(33, nrow(data_testar))) {
 #   teste[[w]] <- offline_data_phase(.data = data_testar[w, ])
 #   print(w)
 #   print(Sys.time())
 # }
-
+# 
+# w <- c(12, # erro: from glmnet C++ code (error code 7777); All used predictors have zero variance
+#        13, # erro: y is constant; gaussian glmnet fails at standardization step
+#        14, # erro: y is constant; gaussian glmnet fails at standardization step
+#        32  # erro: need at least two non-NA values to interpolate
+#        )
 
 # offline meta_learner phase ----
 t0 <- Sys.time()
@@ -64,60 +69,9 @@ saveRDS(m4_metalearner, file = 'db/m4_metalearner.rds', compress = FALSE)
 
 # online phase ----
 t0 <- Sys.time()
-m4_data <- onlin
-
-
-# remove ----
-
-# generate meta-learner 
-    ## create training data
-meta_data <- m4_data %>%
-  pull(meta) %>% bind_rows() %>%
-  pull(meta_matrix) %>% bind_rows() %>%
-  replace(is.na(.), 0)
-
-    ## balance classes
-meta_balanced <- meta_data %>%
-  group_by(best_method) %>%
-  sample_n(364, replace = T)
-
-    ## fit classifier ###
-t0 <- Sys.time()
-# m4_metalearner <- train_meta(meta_data, n_folds = 3, n_cores = 3)
-m4_metalearner <- train_meta(meta_balanced, n_folds = 5, n_cores = 3)
+m4_temp <- online_phase(.data = m4_data,
+                        .learner = m4_metalearner,
+                        .n_cores = 3,
+                        .series_per_chunk = 5)
 Sys.time() - t0
-
-saveRDS(m4_metalearner, file = 'db/m4_metalearner.rds', compress = FALSE)
-
-# evaluate predictions (classes that are being assigned)
-.data <- m4_data %>% sample_n(100)
-.learner <- m4_metalearner
-.new_meta <- .data %>%
-  pull(meta) %>% bind_rows() %>%
-  pull(meta_matrix) %>% bind_rows() %>%
-  replace(is.na(.), 0)
-
-.data %>%
-  # mutate(learner_label = predict(.learner, new_data = .new_meta)) %>% 
-  count(learner_label) %>% mutate(freq = n / sum(n))
-
-meta_data %>% count(best_method) %>% mutate(freq = n / sum(n))
-
-# ## not a very honest review (different series), but an approximation
-# .data %>%
-#   mutate(
-#     learner_label = unlist(predict(.learner, new_data = .new_meta)),
-#     actual = meta_data$best_method
-#   ) %>% 
-#   count(learner_label, actual) %>%
-#   pivot_wider(names_from = learner_label, values_from = n) %>%
-#   arrange(actual)
-
-
-# generate predictions
-.data <- .data %>% 
-  mutate(learner_label = predict(.learner, new_data = .new_meta)) 
-
-.data %>%
-  mutate(previsoes = purrr::map())
 
